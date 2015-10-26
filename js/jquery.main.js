f8f8dbc5ba7b2f312c354440d4b8268a40812fa4
Ian Channing <ian@klever.co.uk>
2015-10-26 12:57:34 +0100

    Bug fix, prevent button clicking pushing the below text up and down
    1. In index.html, add a class to the div around the buttons and add a div element around the reminder text (this is just for structure though and not to fix anything) and replace all the javascript code with a link to the new file
    2. In css/style.css force a fixed height on the buttons div to prevent the padding changes when clicking a button to affect the below text
    3. Delete js/howler.min.js as it's not used anymore
    4. Add js/jquery.main.js to keep the javascript code separate from the HTML

---
+++ js/jquery.main.js
@@ -0,0 +1,30 @@
+(function($) {
+    $('#chker').chker();
+
+    var checkToggle = function(stopButton, startButton) {
+        return $('#chker').hasClass('ticking') ? stopButton : startButton;
+    };
+
+    var myToggle = function(myButton) {
+        myButton.click().focus().toggleClass('button-activated');
+    };
+
+    var myActive = function(myButton) {
+        myButton.toggleClass('button-activated');
+    };
+
+    $(document).keydown(function(event) {
+        if (event.keyCode == '32') { // space bar
+            event.preventDefault(); // the default being scroll the page down
+            myActive(checkToggle($('#chker .stop'), $('#chker .start')));
+        }
+    });
+
+    $(document).keyup(function(event) {
+        if (event.keyCode == '32') { // space bar
+            event.preventDefault();
+            myToggle(checkToggle($('#chker .stop'), $('#chker .start')));
+        }
+
+    });
+})(jQuery);
